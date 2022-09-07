da1f8333b2dbe3192b9dbe298ffc414d01b2c1bf
GOLDENWISE <abdamadhafiz13@gmail.com>
2022-09-07 17:50:51 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -10,5 +10,10 @@
     <h1>Hello World</h1>
     <p>Ini git remote dari git local (bukan hasil cloning cloud github)</p>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, quos! Quae, est eligendi repellendus, voluptates quia, quo nemo cumque accusantium vero dignissimos culpa provident qui tenetur amet maiores deleniti obcaecati.</p>
+    <ul>
+        <li>Lorem ipsum dolor sit.</li>
+        <li>Lorem ipsum dolor sit.</li>
+        <li>Lorem ipsum dolor sit.</li>
+    </ul>
 </body>
 </html>
